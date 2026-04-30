e4d8cf785c81f7bdb3d03d8957bfbabe48bf8808
Laird Kruger <accounts@lairdkruger.com>
2026-04-30 13:49:13 +1000

    Update astro.yml

--- frontend/astro.config.mjs
+++ frontend/astro.config.mjs
@@ -2,4 +2,7 @@
 import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  site: process.env.ASTRO_SITE,
+  base: process.env.ASTRO_BASE,
+});
